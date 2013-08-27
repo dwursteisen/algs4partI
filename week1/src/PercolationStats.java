a1ce0f93f7155e58949a3791cda9b4f67ac199d9
David Wursteisen <david.wursteisen+github@gmail.com>
2013-08-27 17:27:44 +0200

    Ajout des stats

--- week1/src/PercolationStats.java
+++ week1/src/PercolationStats.java
@@ -1,34 +1,75 @@
+import java.util.Locale;
+
 /**
  * Created by david.wursteisen on 27/08/13.
  */
 public class PercolationStats {
+
+    public static final double CONFIDENCE_INTERVAL = 1.96;
+    private final double[] thresholds;
+
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
+        thresholds = new double[T];
+        for (int i = 0; i < T; i++) {
+            thresholds[i] = doSimulation(N);
+        }
     }
 
     // test client, described below
     public static void main(String[] args) {
+        int N = 10;
+        if (args.length >= 1) {
+            N = Integer.parseInt(args[0]);
+        }
+        int T = 10;
+        if (args.length >= 2) {
+            T = Integer.parseInt(args[1]);
+        }
 
+        PercolationStats stats = new PercolationStats(N, T);
+        // mean = 0.666925
+        // stddev = 0.11776536521033558
+        // 95% confidence interval = 0.6646167988418774, 0.6692332011581226
+        StdOut.println(String.format(Locale.ENGLISH, "mean                    = %f", stats.mean()));
+        StdOut.println(String.format(Locale.ENGLISH, "stddev                  = %f", stats.stddev()));
+        StdOut.println(String.format(Locale.ENGLISH, "95%% confidence interval = %f, %f", stats.confidenceLo(), stats.confidenceHi()));
+
+    }
+
+    private double doSimulation(final int N) {
+        Percolation percolation = new Percolation(N);
+        int numberOfSiteOpenned = 0;
+        do {
+            int i = StdRandom.uniform(N) + 1;
+            int j = StdRandom.uniform(N) + 1;
+            if (percolation.isOpen(i, j)) { // already openned => NEXT !!
+                continue;
+            }
+            percolation.open(i, j);
+            numberOfSiteOpenned++;
+        } while (!percolation.percolates());
+
+        return numberOfSiteOpenned / (N * N * 1.0);
     }
 
     // sample mean of percolation threshold
     public double mean() {
-
-        return 0.0;
+        return StdStats.mean(thresholds);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0.0;
+        return StdStats.stddev(thresholds);
     }
 
     // returns lower bound of the 95% confidence interval
     public double confidenceLo() {
-        return 0.0;
+        return mean() - ((CONFIDENCE_INTERVAL * stddev()) / Math.sqrt(thresholds.length));
     }
 
     // returns upper bound of the 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return mean() + ((CONFIDENCE_INTERVAL * stddev()) / Math.sqrt(thresholds.length));
     }
 }
