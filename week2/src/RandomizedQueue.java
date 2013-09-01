1f37ac25496e49b93e4ed25546c5d91846736c50
David Wursteisen <david.wursteisen+git@gmail.com>
2013-09-02 00:01:06 +0200

    implementation naive de RandomizedQueue

---
+++ week2/src/RandomizedQueue.java
@@ -0,0 +1,96 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 01/09/13
+ * Time: 23:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class RandomizedQueue<ITEM_TYPE> implements Iterable<ITEM_TYPE> {
+    private  Object[] items = new Object[5];
+    private int size = 0;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+    }
+
+    // is the queue empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the queue
+    public int size() {
+        return size;
+    }
+
+    // add the item
+    public void enqueue(ITEM_TYPE item) {
+        checkIsNull(item);
+        items[size++] = item;
+        if(items.length != size) {
+            return;
+        }
+        items = Arrays.copyOf(items, size * 2);
+    }
+
+    private void checkIsNull(ITEM_TYPE item) {
+        if(item == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    // delete and return a random item
+    public ITEM_TYPE dequeue() {
+        checkIsEmpty();
+        if(items.length >= size * 4) {
+            items = Arrays.copyOf(items, size * 2);
+        }
+        return (ITEM_TYPE) items[StdRandom.uniform(size--)];
+    }
+
+    // return (but do not delete) a random item
+    public ITEM_TYPE sample() {
+        checkIsEmpty();
+        return (ITEM_TYPE) items[StdRandom.uniform(size)];
+    }
+
+    private void checkIsEmpty() {
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<ITEM_TYPE> iterator() {
+        return new RandomIterator(items);
+    }
+
+    private class RandomIterator implements Iterator<ITEM_TYPE> {
+
+        private Object[] items;
+        private int current = 0;
+
+        private RandomIterator(Object[] items) {
+            this.items = Arrays.copyOf(items, size);
+            StdRandom.shuffle(this.items);
+        }
+        @Override
+        public boolean hasNext() {
+            return current < this.items.length;
+        }
+
+        @Override
+        public ITEM_TYPE next() {
+            return (ITEM_TYPE) items[current++];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ week2/test/RandomizedQueueTest.java
@@ -0,0 +1,93 @@
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: david
+ * Date: 01/09/13
+ * Time: 23:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class RandomizedQueueTest {
+
+    @Test
+    public void should_add_element() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("hello");
+        queue.enqueue("world");
+        assertEquals(2, queue.size());
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void should_be_empty() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        assertEquals(0, queue.size());
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void should_throw_npe_with_null() {
+        new RandomizedQueue<String>().enqueue(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void should_throw_nse_with_dequeue() {
+        new RandomizedQueue<String>().dequeue();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void should_throw_nse_with_sample() {
+        new RandomizedQueue<String>().sample();
+    }
+
+    @Test
+    public void should_enque_lot_of_stuff() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+        queue.enqueue("4");
+        queue.enqueue("5");
+        queue.enqueue("6");
+        queue.enqueue("7");
+        queue.enqueue("8");
+        queue.enqueue("9");
+        queue.enqueue("10");
+        assertEquals(10, queue.size());
+    }
+
+    @Test
+    public void should_sample_item() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("1");
+        assertEquals("1", queue.sample());
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void should_dequeue() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("1");
+        assertEquals("1", queue.dequeue());
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void should_iterate() {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("1");
+        Iterator<String> iterator = queue.iterator();
+
+        assertTrue(iterator.hasNext());
+        assertEquals("1", iterator.next());
+        assertFalse(iterator.hasNext());
+    }
+
+}
